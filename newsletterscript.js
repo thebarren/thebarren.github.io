b96a778d0673a41c39c478172676586087a5ded5
thebarren <65812662+thebarren@users.noreply.github.com>
2026-03-12 10:23:07 +0530

    Add files via upload

--- newsletterscript.js
+++ newsletterscript.js
@@ -1,23 +1,29 @@
-document.addEventListener("DOMContentLoaded", function() {
+function initNewsletter() {
 
   const form = document.getElementById("subscribe-form");
 
+  if (!form) {
+    setTimeout(initNewsletter, 200); // try again shortly
+    return;
+  }
+
   form.addEventListener("submit", function(e) {
+
     e.preventDefault();
 
     const data = new FormData(form);
 
-    fetch("https://script.google.com/macros/s/AKfycbwG8EfnDAV0piDRJMDMmsvBYE-wODLWGZfUJ254cvFvCu4Y2RXA5WZIUGeEpFH3RZgw/exec", {
+    fetch("https://script.google.com/macros/s/AKfycbyQzo52hUyl5r-aOcedR9y0TVBqLhHf0esOgvwBITpJhek6YtDptwi7F2Xkp8Uz6bc8/exec", {
       method: "POST",
-      body: data
-    })
-    .then(() => {
-      document.getElementById("message").innerText = "✓ Thanks for subscribing!";
-      form.reset();
-    })
-    .catch(() => {
-      document.getElementById("message").innerText = "Something went wrong.";
+      body: data,
+      mode: "no-cors"
     });
+
+    document.getElementById("message").innerText = "✓ Thanks for subscribing!";
+    form.reset();
+
   });
 
-});
+}
+
+initNewsletter();
